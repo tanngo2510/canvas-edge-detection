b7f8812bd7a56093c857c1932011412778eaf3d3
Hexros <ngotanvn2001@gmail.com>
2023-05-04 15:27:34 +0700

    update

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const app = express();
-const port = 3000;
 
-app.use(express.static("public"));
+app.set("port", 8080);
 
-app.get("/", (req, res) => {
-  res.sendFile("index.html");
-});
+// app.use(express.static("public"));
 
-app.listen(port, () => {
-  console.log(`Server running at port: ${port}`);
+// app.get("/", (req, res) => {
+//   res.sendFile("index.html");
+// });
+app.use(express.static(__dirname));
+
+var server = app.listen(app.get("port"), function () {
+  var port = server.address().port;
+  console.log(`Open: http://localhost:${port}`);
 });
---
+++ main.js
@@ -0,0 +1,152 @@
+function changeVideo() {
+  //get value from select tag
+  let video = document.getElementById("videos");
+  let name_file = video.options[video.selectedIndex].value;
+  let url = video.options[video.selectedIndex].getAttribute("data-foo");
+  let text = video.options[video.selectedIndex].text;
+  document.getElementById("video").src = name_file + ".mp4";
+  document.getElementById("link-video").href = url;
+  document.getElementById("link-video").innerHTML = text;
+}
+
+const video = document.getElementById("video");
+const canvas = document.getElementById("canvas");
+
+video.addEventListener(
+  "play",
+  () => {
+    const ctx = canvas.getContext("2d");
+    setInterval(() => {
+      ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+      let frame = ctx.getImageData(0, 0, canvas.width, canvas.height);
+      frame = grayscale(frame);
+      // frame = sobelGradient(frame);
+      // frame = nonMaxSuppression(frame);
+      // frame = doubleThresholding(frame);
+      // frame = edgeTracking(frame);
+      ctx.putImageData(frame, 0, 0);
+    }, 20);
+  },
+  false
+);
+
+// //grayscale function
+function grayscale(frame) {
+  const data = frame.data;
+  for (let i = 0; i < data.length; i += 4) {
+    const avg = (data[i] + data[i + 1] + data[i + 2]) / 3;
+    data[i] = avg;
+    data[i + 1] = avg;
+    data[i + 2] = avg;
+  }
+  return frame;
+}
+
+// function sobelGradient(frame) {
+//   const data = frame.data;
+//   const sobelX = [
+//     [-1, 0, 1],
+//     [-2, 0, 2],
+//     [-1, 0, 1],
+//   ];
+//   const sobelY = [
+//     [-1, -2, -1],
+//     [0, 0, 0],
+//     [1, 2, 1],
+//   ];
+//   const newFrame = new ImageData(frame.width, frame.height);
+//   for (let i = 0; i < data.length; i += 4) {
+//     let sumX = 0;
+//     let sumY = 0;
+//     for (let j = 0; j < sobelX.length; j++) {
+//       for (let k = 0; k < sobelX[j].length; k++) {
+//         const x = i + sobelX[j][k];
+//         const y = i + sobelY[j][k];
+//         if (x < 0 || x >= data.length || y < 0 || y >= data.length) {
+//           continue;
+//         }
+//         sumX += data[x] * sobelX[j][k];
+//         sumY += data[y] * sobelY[j][k];
+//       }
+//     }
+//     const sum = Math.sqrt(sumX * sumX + sumY * sumY);
+//     newFrame.data[i] = sum;
+//     newFrame.data[i + 1] = sum;
+//     newFrame.data[i + 2] = sum;
+//     newFrame.data[i + 3] = 255;
+//   }
+//   return newFrame;
+// }
+
+// function nonMaxSuppression(frame) {
+//   const data = frame.data;
+//   const newFrame = new ImageData(frame.width, frame.height);
+//   for (let i = 0; i < data.length; i += 4) {
+//     const pixel = data[i];
+//     const prevPixel = data[i - 4];
+//     const nextPixel = data[i + 4];
+//     if (pixel > prevPixel && pixel > nextPixel) {
+//       newFrame.data[i] = pixel;
+//       newFrame.data[i + 1] = pixel;
+//       newFrame.data[i + 2] = pixel;
+//       newFrame.data[i + 3] = 255;
+//     }
+//   }
+//   return newFrame;
+// }
+
+// function doubleThresholding(frame) {
+//   const data = frame.data;
+//   const newFrame = new ImageData(frame.width, frame.height);
+//   for (let i = 0; i < data.length; i += 4) {
+//     const pixel = data[i];
+//     if (pixel < 50) {
+//       newFrame.data[i] = 0;
+//       newFrame.data[i + 1] = 0;
+//       newFrame.data[i + 2] = 0;
+//       newFrame.data[i + 3] = 255;
+//     } else if (pixel > 150) {
+//       newFrame.data[i] = 255;
+//       newFrame.data[i + 1] = 255;
+//       newFrame.data[i + 2] = 255;
+//       newFrame.data[i + 3] = 255;
+//     } else {
+//       newFrame.data[i] = 100;
+//       newFrame.data[i + 1] = 100;
+//       newFrame.data[i + 2] = 100;
+//       newFrame.data[i + 3] = 255;
+//     }
+//   }
+//   return newFrame;
+// }
+
+// function edgeTracking(frame) {
+//   const data = frame.data;
+//   const newFrame = new ImageData(frame.width, frame.height);
+//   for (let i = 0; i < data.length; i += 4) {
+//     const pixel = data[i];
+//     if (pixel == 100) {
+//       newFrame.data[i] = 255;
+//       newFrame.data[i + 1] = 255;
+//       newFrame.data[i + 2] = 255;
+//       newFrame.data[i + 3] = 255;
+//     } else {
+//       newFrame.data[i] = 0;
+//       newFrame.data[i + 1] = 0;
+//       newFrame.data[i + 2] = 0;
+//       newFrame.data[i + 3] = 255;
+//     }
+//   }
+//   return newFrame;
+// }
+
+function drawCanvas() {
+  var ctx = canvas.getContext("2d");
+  ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+}
+
+// video.addEventListener("play", () => {
+//   setInterval(() => {
+//     drawCanvas();
+//   }, 30);
+// });
